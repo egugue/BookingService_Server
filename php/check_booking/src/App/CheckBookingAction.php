719c59de03ff2e596905d2b1be821326a8d25785
Haruyoshi Toyama <duane0728@gmail.com>
2017-01-07 21:59:29 +0900

    Be able to perform CheckBookingAction properly

--- php/check_booking/src/App/CheckBookingAction.php
+++ php/check_booking/src/App/CheckBookingAction.php
@@ -1,9 +1,11 @@
 <?php
 
 use Egugue\BookingSerivce\CheckBooking\Data\Db\LessonsDbDao;
+use Egugue\BookingSerivce\CheckBooking\Data\Firebase\StudentFirebaseDao;
+use Egugue\BookingSerivce\CheckBooking\Data\Push\Notification;
 use Egugue\BookingSerivce\CheckBooking\Data\Web\LessonPageClient;
 use Egugue\BookingSerivce\CheckBooking\Model\Lesson\LessonService;
-use Egugue\BookingSerivce\CheckBooking\Model\Teacher\TeacherId;
+use Egugue\BookingSerivce\CheckBooking\Model\Student\Student;
 
 class CheckBookingAction
 {
@@ -11,8 +13,13 @@ class CheckBookingAction
   private $client;
   private $lessonService;
 
-  function __construct(LessonsDbDao $dbDao = null, LessonPageClient $client = null,
-                       LessonService $lessonService = null)
+  private $studentDao;
+
+  //TODO: Reduce some dependency.
+  function __construct(LessonsDbDao $dbDao = null,
+                       LessonPageClient $client = null,
+                       LessonService $lessonService = null,
+                       StudentFirebaseDao $studentDao = null)
   {
     if ($dbDao === null) {
       $dbDao = new LessonsDbDao();
@@ -23,18 +30,50 @@ class CheckBookingAction
     if ($lessonService === null) {
       $lessonService = new LessonService();
     }
+    if ($studentDao == null) {
+      $studentDao = new StudentFirebaseDao();
+    }
 
     $this->lessonService = $lessonService;
     $this->dbDao = $dbDao;
     $this->client = $client;
+    $this->studentDao = $studentDao;
   }
 
-  function execute(TeacherId $teacherId)
+  function execute()
   {
-    $old = $this->dbDao->selectByTeacherId($teacherId);
-    $new = $this->client->getBy($teacherId);
-    $newlyAvailable = $this->lessonService->extractNewlyAvailableLessons($old, $new);
+    //TODO: Instantiate in the constructor.
+    $notification = new Notification();
+    $allStudents = $this->studentDao->getAll();
 
-    $this->dbDao->insertOrUpdate($new);
+    foreach ($allStudents as $student) {
+      $newlyAvailableList = $this->getNewlyAvailableList($student);
+      if (count($newlyAvailableList) !== 0) {
+        $notification->send($student, $newlyAvailableList);
+      }
+    }
+
+    //TODO: Insert after sending notification.
+    //$this->dbDao->insertOrUpdate($new);
+  }
+
+  private function getNewlyAvailableList(Student $student)
+  {
+    $newlyAvailableList = array();
+
+    foreach ($student->favoriteTeacherIds() as $teacherId) {
+      $old = $this->dbDao->selectByTeacherId($teacherId);
+      $new = $this->client->getBy($teacherId);
+      $newlyAvailable = $this->lessonService->extractNewlyAvailableLessons($old, $new);
+
+      //TODO: Delete it later. Instead of it, we must call this method under the method.
+      $this->dbDao->insertOrUpdate($new);
+
+      if (count($newlyAvailable) !== 0) {
+        $newlyAvailableList += $newlyAvailable;
+      }
+    }
+
+    return $newlyAvailableList;
   }
 }
