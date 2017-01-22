662d20ebbcf15f3a2da7646ee9b92be09efaf662
Haruyoshi Toyama <duane0728@gmail.com>
2017-01-22 22:55:50 +0900

    Add TeacherController

--- scala/de_booker/app/com/egugue/debooker/application/controller/TeacherController.scala
+++ scala/de_booker/app/com/egugue/debooker/application/controller/TeacherController.scala
@@ -2,18 +2,14 @@ package com.egugue.debooker.application.controller
 
 import javax.inject.{Inject, Singleton}
 
+import com.egugue.debooker.application.controller.teacher.TeacherJson
+import play.api.libs.json.Json
 import play.api.mvc._
-import play.libs.Json
 
-/**
-  * Created by htoyama on 2017/01/18.
-  */
 @Singleton
 class TeacherController @Inject() extends Controller {
 
   def get(id: Long) = Action { implicit request =>
-    val list = Map("id" -> id)
-    val added = Json.newArray().add(id)
-    Ok(added.toString)
+    Ok(Json.toJson(TeacherJson(id, "teacher")))
   }
 }
